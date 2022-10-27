94765a72d3a9d73fea69fc977ed32a9657f503ff
Cospis <qozimidris@gmail.com>
2022-10-27 09:43:25 +0100

    Added profile picture, twitter and slack

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Profile from "./Profile";
+function App() {
+  return (
+    <div className="profile_section">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Profile.js
@@ -0,0 +1,20 @@
+import dp from "./dp.jpeg";
+const Profile = () => {
+  return (
+    <div className="dp">
+      <img src={dp} alt="dp" id="profile__img" />
+      <div className="name">
+        {" "}
+        <h1> Qozim Idris</h1>
+      </div>
+      <div className="btn">
+        <button id="twitter">cos_pis</button>
+      </div>
+      <div className="hidden">
+        <button id="slack">Cospis1️⃣</button>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
