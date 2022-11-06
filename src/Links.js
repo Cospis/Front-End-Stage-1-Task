28986b1dd3e7e3c9d686e61570b76baf21f9435c
Cospis <39841495+Cospis@users.noreply.github.com>
2022-11-06 14:01:33 +0100

    Update Links.js

--- src/Links.js
+++ src/Links.js
@@ -19,8 +19,9 @@ const Links = () => {
       <a href="https://books.zuri.team/design-rules " className="book__design">
         <button id="book__design"> Design Books</button>
       </a>
-    <a href="/contact" className="contact"  <button id="contact">  
-  Contact Me  </button>  
+     <a href="/contact" className="contact"  id="contact">
+        <button id="contact"> Contact me</button>
+     
       </a>
     </div>
   );
